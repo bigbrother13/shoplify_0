98765a3aa95fb7c43d10c1c5a599d1b15cbeb29b
Vladimir <gritskevich.vladimir@gmail.com>
2024-10-12 22:50:34 +0200

    add stripe webhooks

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,6 @@ Rails.application.routes.draw do
   resources :products
   resources :checkout, only: [:create]
   post 'checkout/create', to: 'checkout#create', defaults: { format: :html }
+  resources :webhooks, only: [:create]
 
-
-
-
-
-
-
-  # get "up" => "rails/health#show", as: :rails_health_check
-
-  # # Render dynamic PWA files from app/views/pwa/*
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # # Defines the root path route ("/")
-  # # root "posts#index"
 end
