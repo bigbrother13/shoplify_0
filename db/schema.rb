6ac49c07bb384bc99ed693641656314a37319a9c
Vladimir <gritskevich.vladimir@gmail.com>
2024-10-29 23:27:06 +0100

    add_stripe_product_id_to_products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_19_201503) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_27_214114) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_19_201503) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "sales_count", default: 0, null: false
+    t.string "stripe_product_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,6 +31,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_19_201503) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stripe_customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
